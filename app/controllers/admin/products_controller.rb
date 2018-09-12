72b4ff2f61f1683922f8d77044c4c5673b34dadc
vovanlong <vovanlong1997@gmail.com>
2018-09-12 09:13:58 +0700

    true

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductsController < Admin::BaseController
-  before_action :load_product, only: [:edit]
+  before_action :load_product, only: [:edit, :update]
   def index
     @products = Product.joins(:photos)
     
@@ -14,7 +14,6 @@ class Admin::ProductsController < Admin::BaseController
   end
   
   def create
-    name = params[:name]
     @product = current_user.products.build(product_params)
     if @product.save
       if params[:images]
@@ -27,7 +26,22 @@ class Admin::ProductsController < Admin::BaseController
   end
 
   def update
-    
+    name = params[:name]
+    description = params[:description]
+    price = params[:price]
+    quantity = params[:quantity]
+    category_id = params[:category_id]
+    user_id = current_user.id
+    if @product.update_attributes!(:name => name, :description => description, :price => price, :quantity => quantity, :category_id => category_id, :user_id =>user_id)
+      if params[:images]
+        @product.photos.destroy_all
+        params[:images].each do |img|
+          @product.photos.create(image: img[1])
+        end
+      end
+    else
+
+    end
   end
   
   
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :category
   belongs_to :user
-  has_many :reviews
-  has_many :comments
+  has_many :reviews, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :photos, dependent: :destroy
 
   validates :name, presence: true,length: {maximum: 50}
